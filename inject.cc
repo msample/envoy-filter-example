0589b9749ab46111f5d0df6a15b70397105f356f
Mike Sample <msample@users.noreply.github.com>
2017-07-15 22:13:26 -0700

    Cookie triggers and removal (but not injection) now supported by using header names like 'cookie.<cookie-name>'. Also checks that cluster_name cluster actually exists in the config - fails startup if it doesn't

--- inject.cc
+++ inject.cc
@@ -10,26 +10,23 @@
 namespace Envoy {
 namespace Http {
 
-
 // called for gRPC call to InjectHeader
 void InjectFilter::onCreateInitialMetadata(Http::HeaderMap& ) {
-  std::cout << "called onCreateInitialMetadata on icb: " << this << std::endl;
 }
 
 // called for gRPC call to InjectHeader
 void InjectFilter::onReceiveInitialMetadata(Http::HeaderMapPtr&&) {
-  std::cout << "called on onReceiveInitialMetadata on icb: " << this << std::endl;
 }
 
 // called for gRPC call to InjectHeader
-void InjectFilter::onReceiveMessage(std::unique_ptr<inject::InjectResponse>&& resp)  {
+void InjectFilter::onReceiveMessage(std::unique_ptr<inject::InjectResponse>&& resp) {
   std::cout << "called on onReceiveMessage on icb: " << this << std::endl;
   if (req_) {
     std::cout << "onRecvMsmg non-nil req_ on  " << this << std::endl;
     req_->resetStream();
     req_ = 0;
   }
-  std::map<std::string,std::string> inject_hdrs_; 
+  std::map<std::string,std::string> inject_hdrs_;
   for (int i = 0; i < resp->headers_size(); ++i) {
     const inject::Header& h = resp->headers(i);
     inject_hdrs_.insert(std::pair<std::string,std::string>(h.key(), h.value()));
@@ -43,9 +40,16 @@ void InjectFilter::onReceiveMessage(std::unique_ptr<inject::InjectResponse>&& re
     }
   }
 
+  // remove any headers named in the config
   for (const Http::LowerCaseString& element : config_->remove_headers()) {
     hdrs_->remove(element);
   }
+
+  // remove any cookies as defined in config
+  for (const std::string& name: config_->remove_cookie_names()) {
+    removeNamedCookie(name, *hdrs_);
+  }
+
   callbacks_->continueDecoding();
   std::cout << "exiting onReceiveMessage on icb: " << this << std::endl;
 }
@@ -77,74 +81,93 @@ FilterHeadersStatus InjectFilter::decodeHeaders(HeaderMap& headers, bool) {
     }
   }
 
+  inject::InjectRequest ir;
   bool triggered = false;
-  std::vector<const Http::HeaderEntry*> to_send;
   for (const Http::LowerCaseString& element : config_->trigger_headers()) {
     const Http::HeaderEntry* h = headers.get(element);
     if (h) {
-      if (!triggered) {
-        triggered = true;
-        to_send.reserve(config_->trigger_headers().size() + config_->include_headers().size());
-      }
-      to_send.push_back(h);
+      triggered = true;
+      inject::Header* h =  ir.mutable_inputheaders()->Add();
+      h->set_key(h->key().c_str());
+      h->set_value(h->value().c_str());
     }
+  }
+
+  // check for cookies with names that trigger injection
+  for (const std::string& name: config_->trigger_cookie_names()) {
+    std::string cookie_value = Http::Utility::parseCookieValue(headers, name);
+    if (cookie_value.empty()) {
+      continue;
+    }
+    triggered = true;
+    inject::Header* h =  ir.mutable_inputheaders()->Add();
+    h->set_key(name);
+    h->set_value(cookie_value);
   }
 
   if (!triggered) {
     return FilterHeadersStatus::Continue;
   }
+  std::cout << "Inject trigger matched: " << this << std::endl;
 
+  // add non-triggering headers of interest to inject request
   for (const Http::LowerCaseString& element : config_->include_headers()) {
     const Http::HeaderEntry* h = headers.get(element);
     if (h) {
-      to_send.push_back(h);
+      inject::Header* h =  ir.mutable_inputheaders()->Add();
+      h->set_key(h->key().c_str());
+      h->set_value(h->value().c_str());
     }
   }
-  std::cout << "Inject trigger matched" << std::endl;
 
-  inject::InjectRequest ir;
-
-  for (const Http::HeaderEntry* element : to_send) {
-    inject::Header* h =  ir.mutable_inputheaders()->Add();
-    h->set_key(element->key().c_str());
-    h->set_value(element->value().c_str());
-  }
-
+  // add names of headers we want/allow injected to inject request
   for (const Http::LowerCaseString& element : config_->inject_headers()) {
     ir.add_injectheadernames(element.get());
   }
 
-  //  issues here - if don't reset stream in this fcn (we can't if we want the reply) we get seg fault.
-  //std::unique_ptr<Grpc::AsyncClientStream<inject::InjectRequest>> x{config_->inject_client()->start(config_->method_descriptor(), *this, std::chrono::milliseconds(10))};
-  //req_ = std::move(x);
-  //req_ = std::move(config_->inject_client()->start(config_->method_descriptor(), *this, std::chrono::milliseconds(10)));
-  req_ = config_->inject_client()->start(config_->method_descriptor(), *this, std::chrono::milliseconds(10));
-  req_->sendMessage(ir);
-  req_->closeStream();
- 
-  // no segv if do this... but no progress either
-  //req_->resetStream();
-  callbacks_->route(); // see if we can init router to avoid explosion on reset from missing grpc svc
+
+  // issues here - if don't reset stream in this fcn (we can't if we want the reply) we get seg fault.
+  // req_ = config_->inject_client()->start(config_->method_descriptor(), *this, std::chrono::milliseconds(10));
+  // req_->sendMessage(ir);
+  // req_->closeStream();
+  // no segv if reset... but no progress either
+  // req_->resetStream();
+
+  // START TEMP SECTION - REMOVE WHEN gRPC working
+  // demo - fake out injected headers
+  for (const Http::LowerCaseString& element : config_->inject_headers()) {
+    headers.addStaticKey(element, "example-injected-header-value");
+  }
+
+  // remove any cookies as defined in config
+  for (const std::string& name: config_->remove_cookie_names()) {
+    removeNamedCookie(name, headers);
+  }
+
+  // remove any headers as defined in config
+  for (const Http::LowerCaseString& name: config_->remove_headers()) {
+    headers.remove(name);
+  }
+
+  // pretend we're done
+  return FilterHeadersStatus::Continue;
+  // END TEMP SECTION - REMOVE WHEN gRPC working
 
   hdrs_ = &headers;
   // give control back to event loop so gRPC inject response can be received
   // FIXME: set timeout with dispatcher to either contine or fail the original call
-  std::cout << "exiting decode headers filter method, waiting for grpc response" << std::endl;
   return FilterHeadersStatus::StopIteration;
 }
 
 FilterDataStatus InjectFilter::decodeData(Buffer::Instance&, bool) {
-  std::cout << "decode data called on: " << this << std::endl;
   return FilterDataStatus::Continue;
 }
 
 FilterTrailersStatus InjectFilter::decodeTrailers(HeaderMap&) {
-  std::cout << "decode trailers called on: " << this << std::endl;
   return FilterTrailersStatus::Continue;
 }
 
 void InjectFilter::setDecoderFilterCallbacks(StreamDecoderFilterCallbacks& callbacks) {
-  std::cout << "decoder filter callbacks set on: " << this << std::endl;
   callbacks_ = &callbacks;
 }
 
@@ -156,7 +179,81 @@ void InjectFilter::onDestroy() {
   }
 }
 
+static const Http::LowerCaseString cookie_hdr_name{"cookie"};
 
+// Removes the cookie header from the headers and replaces it with one
+// whose value does not include the named cookie(s).
+void InjectFilter::removeNamedCookie(const std::string& key, Http::HeaderMap& headers) {
+  const Http::HeaderEntry* h = headers.get(cookie_hdr_name);
+  if (!h) {
+    return;
+  }
+  std::string cookie_hdr_value(h->value().c_str());
+
+  // optimization - no mutation if doesn't exist
+  if (cookie_hdr_value.find(key + "=") == std::string::npos) {
+    return;
+  }
+  // modify cookie hdr value
+  removeNamedCookie(key, cookie_hdr_value);
+
+  headers.remove(cookie_hdr_name);  // addStaticKey appends unless remove first
+  if (!cookie_hdr_value.empty()) {
+    headers.addStaticKey(cookie_hdr_name, cookie_hdr_value);
+  }
+}
+
+// Alters given cookie header value by removing cookie(s) of the given
+// name (case sensitive).  Assumes cookie header value looks like
+// "foo=fooval; bar=barval; baz=bazval",
+// "foo=fooval;bar=barval;baz=bazval" or a combination of that spacing
+//
+void InjectFilter::removeNamedCookie(const std::string& cookie_name, std::string& cookie_hdr_value) {
+  size_t start_idx = cookie_hdr_value.find(cookie_name + "=");
+
+  // iterates in case the same cookie name appears multiple times
+  while (start_idx != std::string::npos) {
+    size_t end_prev_cookie_idx = std::string::npos;
+    bool is_first = true;
+    if (start_idx != 0) {
+      end_prev_cookie_idx = cookie_hdr_value.find_last_not_of(" ;", start_idx-1); // skip semi-colon too!
+      if (std::string::npos != end_prev_cookie_idx) {
+        is_first = false;
+      }
+    }
+    size_t end_idx = cookie_hdr_value.find(";", start_idx + cookie_name.length() + 2);
+    bool is_last = true;
+    size_t start_next_cookie_idx = std::string::npos;
+    if (std::string::npos != end_idx) {
+      start_next_cookie_idx = cookie_hdr_value.find_first_not_of(" ", end_idx+1);
+      // only consider non-last if there's more than whitespace after the semi-colon
+      if (start_next_cookie_idx != std::string::npos) {
+        is_last = false;
+      }
+    }
+
+    // this is only cookie value, blow away entire cookie value & return
+    if (is_first && is_last) {
+      cookie_hdr_value.clear();
+      return;
+    }
+
+    // it's the last cookie. erase & return (we've found 'em all)
+    if (is_last) {
+      cookie_hdr_value.erase(end_prev_cookie_idx + 1);
+      return;
+    }
+
+    // erase first cookie
+    if (is_first) {
+      cookie_hdr_value.erase(0, start_next_cookie_idx);
+    } else {
+      // erase middle cookie
+      cookie_hdr_value.erase(end_prev_cookie_idx + 1, end_idx - end_prev_cookie_idx - 1);
+    }
+    start_idx = cookie_hdr_value.find(cookie_name + "="); // any more?
+  }
+}
 
 
 } // Http
